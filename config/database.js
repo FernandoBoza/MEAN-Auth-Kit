8e3f2f37f542ebdd96994c3799ec503a7700d39b
Fernando Boza <hi@fernandoboza.com>
2017-04-05 22:24:02 -0400

    linked db

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-    database: 'mongodb://localhost:27017/meanAuth',
+    database: 'mongodb://fernando:loki1989@ds153730.mlab.com:53730/meanauthapp',
     secret: 'yoursecret'
 };
